068ae734ce422621156808349d23cfabb1d9b3c6
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2020-07-14 15:00:40 -0300

    Se sube app

---
+++ script/script.js
@@ -0,0 +1,199 @@
+// OBJETO CON LAS PROPIEDADES DE LA CALCULADORA
+
+var p = {
+    teclas: document.querySelectorAll('#calculadora ul li'),
+    accion: null,
+    digito: null,
+    operaciones: document.querySelector('#operaciones'),
+    contadorSignos: 0,
+    contadorDecimal: false,
+    ejecutadorResult: false
+
+
+}
+
+
+// OBJETO CON LOS METODOS DE LA CALCULADORA
+
+var m = {
+    inicio: function() {
+        for(var i = 0; i < p.teclas.length; i++){
+            p.teclas[i].addEventListener('click', m.oprimirTecla)
+        };
+
+        document.addEventListener('keydown', m.oprimir);
+    },
+
+    oprimir: function(tecla) {
+        p.accion = null;
+        p.digito = null;
+
+        if(tecla.keyCode == 48 || tecla.keyCode == 96){
+            p.accion = "numero";
+            p.digito = 0;
+        };
+
+        if(tecla.keyCode == 49 || tecla.keyCode == 97){
+            p.accion = "numero";
+            p.digito = 1;
+        };
+
+        if(tecla.keyCode == 50 || tecla.keyCode == 98){
+            p.accion = "numero";
+            p.digito = 2;
+        };
+
+        if(tecla.keyCode == 51 || tecla.keyCode == 99){
+            p.accion = "numero";
+            p.digito = 3;
+        };
+
+        if(tecla.keyCode == 52 || tecla.keyCode == 100){
+            p.accion = "numero";
+            p.digito = 4;
+        };
+
+        if(tecla.keyCode == 53 || tecla.keyCode == 101){
+            p.accion = "numero";
+            p.digito = 5;
+        };
+
+        if(tecla.keyCode == 54 || tecla.keyCode == 102){
+            p.accion = "numero";
+            p.digito = 6;
+        };
+
+        if(tecla.keyCode == 55 || tecla.keyCode == 103){
+            p.accion = "numero";
+            p.digito = 7;
+        };
+
+        if(tecla.keyCode == 56 || tecla.keyCode == 104){
+            p.accion = "numero";
+            p.digito = 8;
+        };
+
+        if(tecla.keyCode == 57 || tecla.keyCode == 105){
+            p.accion = "numero";
+            p.digito = 9;
+        };
+
+        if(tecla.keyCode == 187 || tecla.keyCode == 107){
+            p.accion = "signo";
+            p.digito = '+';
+        };
+
+        if(tecla.keyCode == 189 || tecla.keyCode == 109){
+            p.accion = "signo";
+            p.digito = '-';
+        };
+
+        if(tecla.keyCode == 88 || tecla.keyCode == 106){
+            p.accion = "signo";
+            p.digito = '*';
+        };
+
+        if(tecla.keyCode == 111){
+            p.accion = "signo";
+            p.digito = '/';
+        };
+
+        if(tecla.keyCode == 190 || tecla.keyCode == 110){
+            p.accion = "decimal";
+            p.digito = '.';
+        };
+
+        if(tecla.keyCode == 13){
+            p.accion = "igual";
+        };
+
+        if(tecla.keyCode == 8){
+            m.borrar();
+        };
+
+
+        m.calculadora(p.accion, p.digito)
+    },
+
+    oprimirTecla: function(event){
+        p.accion = event.target.getAttribute('class'),
+        p.digito = event.target.innerHTML;
+
+        m.calculadora(p.accion, p.digito)
+    },
+
+    calculadora: function(event, digito){
+        switch(event){
+            case 'numero':
+
+                p.contadorSignos = 0
+                
+                if(p.operaciones.innerHTML == 0 ){
+                    p.operaciones.innerHTML = digito
+                }else{
+
+                    if(p.ejecutadorResult){
+                        p.ejecutadorResult = false;
+                        p.operaciones.innerHTML = digito;
+                    }else{
+                        p.operaciones.innerHTML += digito
+                    }
+                }
+
+                break;
+
+            case 'signo':
+                p.contadorSignos++
+                
+                if(p.contadorSignos == 1){
+
+                    if(p.operaciones.innerHTML == 0){
+                        p.operaciones.innerHTML = 0;
+                    }else{
+                        p.operaciones.innerHTML += digito;
+                        p.contadorDecimal = false;
+                        p.ejecutadorResult = false;
+                    }
+                }
+                
+                break;
+            
+            case 'decimal':
+
+                if(p.operaciones.innerHTML == 0){
+                    p.contadorDecimal = true;
+                }else{
+                    p.contadorDecimal = false;
+                    if(!p.contadorDecimal && p.contadorSignos != 1){
+                        
+                        p.operaciones.innerHTML += digito;
+                        p.contadorDecimal = true;
+                        p.ejecutadorResult = false;
+                    }
+                }
+                
+                break;
+
+            case 'igual':
+                p.operaciones.innerHTML = eval(p.operaciones.innerHTML);
+
+                var expresion = /./g;
+
+                if(!expresion.test(p.operaciones.innerHTML)){
+                    p.contadorDecimal = true;
+                }
+
+                p.ejecutadorResult = true;
+                
+                break;
+        }
+    },
+
+    borrar: function(){
+        p.operaciones.innerHTML = 0;
+        p.contadorDecimal = false;
+       
+    }
+}
+
+m.inicio();
